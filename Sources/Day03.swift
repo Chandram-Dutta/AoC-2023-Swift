ccbc28e294a4217c0c99cc3010e247943bacb7a0
Chandram-Dutta <chandramdutta2004@gmail.com>
2023-12-04 02:31:10 +0530

    code formatted

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -178,160 +178,160 @@ struct Day03: AdventDay {
       let line = Array((String(lines)))
       arr.append(line)
     }
-      for i in 0..<arr.count {
-          for j in 0..<arr[i].count {
-              if (arr[i][j] == "*"){
-                  var nums = [Int]()
-                  
-                    if arr[i - 1][j] != "." {
-                      var num = ""
-                      var k = 0
-                      var run = true
-                      while run {
-                        if j - k >= 0 && arr[i - 1][j - k] != "." {
-                          num = String(arr[i - 1][j - k]).appending(num)
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                      k = 1
-                      run.toggle()
-                      while run {
-                        if j + k <= (arr[i].count) - 1 && arr[i - 1][j + k] != "." {
-                          num.append(arr[i - 1][j + k])
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                        nums.append(Int(num)!)
-                    } else {
-                      if arr[i - 1][j - 1] != "." {
-                        var num = 0
-                        var k = 1
-                        var run = true
-                        while run {
-                          if j - k >= 0 && arr[i - 1][j - k] != "." {
-                            num =
-                              num
-                              + (Int(String(arr[i - 1][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
-                          } else {
-                            run.toggle()
-                          }
-                          k += 1
-                        }
-                          nums.append(num)
-                      }
-                      if arr[i - 1][j + 1] != "." {
-                        var num = ""
-                        var k = 1
-                        var run = true
-                        while run {
-                          if j + k <= (arr[i].count) - 1 && arr[i - 1][j + k] != "." {
-                            num.append(String(arr[i - 1][j + k]))
-                          } else {
-                            run.toggle()
-                          }
-                          k += 1
-                        }
-                          nums.append(Int(num)!)
-                      }
-                    }
+    for i in 0..<arr.count {
+      for j in 0..<arr[i].count {
+        if arr[i][j] == "*" {
+          var nums = [Int]()
 
-                    if arr[i][j - 1] != "." {
-                      var num = 0
-                      var k = 1
-                      var run = true
-                      while run {
-                        if j - k >= 0 && arr[i][j - k] != "." {
-                          num =
-                            num
-                            + (Int(String(arr[i][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                        nums.append(num)
-                    }
-                    if arr[i][j + 1] != "." {
-                      var num = ""
-                      var k = 1
-                      var run = true
-                      while run {
-                        if j + k <= (arr[i].count) - 1 && arr[i][j + k] != "." {
-                          num.append(String(arr[i][j + k]))
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                        nums.append(Int(num)!)
-                    }
-
-                    if arr[i + 1][j] != "." {
-                      var num = ""
-                      var k = 0
-                      var run = true
-                      while run {
-                        if j - k >= 0 && arr[i + 1][j - k] != "." {
-                          num = String(arr[i + 1][j - k]).appending(num)
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                      k = 1
-                      run.toggle()
-                      while run {
-                        if j + k <= (arr[i].count) - 1 && arr[i + 1][j + k] != "." {
-                          num.append(arr[i + 1][j + k])
-                        } else {
-                          run.toggle()
-                        }
-                        k += 1
-                      }
-                        nums.append(Int(num)!)
-                    } else {
-                      if arr[i + 1][j - 1] != "." {
-                        var num = 0
-                        var k = 1
-                        var run = true
-                        while run {
-                          if j - k >= 0 && arr[i + 1][j - k] != "." {
-                            num =
-                              num
-                              + (Int(String(arr[i + 1][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
-                          } else {
-                            run.toggle()
-                          }
-                          k += 1
-                        }
-                          nums.append(num)
-                      }
-                      if arr[i + 1][j + 1] != "." {
-                        var num = ""
-                        var k = 1
-                        var run = true
-                        while run {
-                          if j + k <= (arr[i].count) - 1 && arr[i + 1][j + k] != "." {
-                            num.append(String(arr[i + 1][j + k]))
-                          } else {
-                            run.toggle()
-                          }
-                          k += 1
-                        }
-                          nums.append(Int(num)!)
-                      
-                    }
-                  }
-                  if nums.count == 2 {
-                      sum += (nums[0]*nums[1])
-                  }
+          if arr[i - 1][j] != "." {
+            var num = ""
+            var k = 0
+            var run = true
+            while run {
+              if j - k >= 0 && arr[i - 1][j - k] != "." {
+                num = String(arr[i - 1][j - k]).appending(num)
+              } else {
+                run.toggle()
               }
+              k += 1
+            }
+            k = 1
+            run.toggle()
+            while run {
+              if j + k <= (arr[i].count) - 1 && arr[i - 1][j + k] != "." {
+                num.append(arr[i - 1][j + k])
+              } else {
+                run.toggle()
+              }
+              k += 1
+            }
+            nums.append(Int(num)!)
+          } else {
+            if arr[i - 1][j - 1] != "." {
+              var num = 0
+              var k = 1
+              var run = true
+              while run {
+                if j - k >= 0 && arr[i - 1][j - k] != "." {
+                  num =
+                    num
+                    + (Int(String(arr[i - 1][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
+                } else {
+                  run.toggle()
+                }
+                k += 1
+              }
+              nums.append(num)
+            }
+            if arr[i - 1][j + 1] != "." {
+              var num = ""
+              var k = 1
+              var run = true
+              while run {
+                if j + k <= (arr[i].count) - 1 && arr[i - 1][j + k] != "." {
+                  num.append(String(arr[i - 1][j + k]))
+                } else {
+                  run.toggle()
+                }
+                k += 1
+              }
+              nums.append(Int(num)!)
+            }
           }
+
+          if arr[i][j - 1] != "." {
+            var num = 0
+            var k = 1
+            var run = true
+            while run {
+              if j - k >= 0 && arr[i][j - k] != "." {
+                num =
+                  num
+                  + (Int(String(arr[i][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
+              } else {
+                run.toggle()
+              }
+              k += 1
+            }
+            nums.append(num)
+          }
+          if arr[i][j + 1] != "." {
+            var num = ""
+            var k = 1
+            var run = true
+            while run {
+              if j + k <= (arr[i].count) - 1 && arr[i][j + k] != "." {
+                num.append(String(arr[i][j + k]))
+              } else {
+                run.toggle()
+              }
+              k += 1
+            }
+            nums.append(Int(num)!)
+          }
+
+          if arr[i + 1][j] != "." {
+            var num = ""
+            var k = 0
+            var run = true
+            while run {
+              if j - k >= 0 && arr[i + 1][j - k] != "." {
+                num = String(arr[i + 1][j - k]).appending(num)
+              } else {
+                run.toggle()
+              }
+              k += 1
+            }
+            k = 1
+            run.toggle()
+            while run {
+              if j + k <= (arr[i].count) - 1 && arr[i + 1][j + k] != "." {
+                num.append(arr[i + 1][j + k])
+              } else {
+                run.toggle()
+              }
+              k += 1
+            }
+            nums.append(Int(num)!)
+          } else {
+            if arr[i + 1][j - 1] != "." {
+              var num = 0
+              var k = 1
+              var run = true
+              while run {
+                if j - k >= 0 && arr[i + 1][j - k] != "." {
+                  num =
+                    num
+                    + (Int(String(arr[i + 1][j - k]))! * Int(truncating: pow(10, k - 1) as NSNumber))
+                } else {
+                  run.toggle()
+                }
+                k += 1
+              }
+              nums.append(num)
+            }
+            if arr[i + 1][j + 1] != "." {
+              var num = ""
+              var k = 1
+              var run = true
+              while run {
+                if j + k <= (arr[i].count) - 1 && arr[i + 1][j + k] != "." {
+                  num.append(String(arr[i + 1][j + k]))
+                } else {
+                  run.toggle()
+                }
+                k += 1
+              }
+              nums.append(Int(num)!)
+
+            }
+          }
+          if nums.count == 2 {
+            sum += (nums[0] * nums[1])
+          }
+        }
       }
+    }
     return sum
   }
 }
